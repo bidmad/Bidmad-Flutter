831f0c7357a6153a5d164949dcc4d4d7810b4695
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2026-04-30 16:17:49 +0900

    Remove bidmad_plugin in favor of bidmad_plugin_newscash

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -6,12 +6,6 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<bidmad_plugin/bidmad_plugin.h>)
-#import <bidmad_plugin/bidmad_plugin.h>
-#else
-@import bidmad_plugin;
-#endif
-
 #if __has_include(<bidmad_plugin_newscash/bidmad_plugin_newscash.h>)
 #import <bidmad_plugin_newscash/bidmad_plugin_newscash.h>
 #else
@@ -21,7 +15,6 @@
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [bidmad_plugin registerWithRegistrar:[registry registrarForPlugin:@"bidmad_plugin"]];
   [bidmad_plugin_newscash registerWithRegistrar:[registry registrarForPlugin:@"bidmad_plugin_newscash"]];
 }
 
